92e1e68f045d435bfb6a6eb0a3641dacb197988c
Segge <seggbenga@gmail.com>
2022-12-24 03:17:17 +0100

    add executable files

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""To define a square """
+
+
+class Square:
+    """Representing a square"""
+
+    def __init__(self, size):
+
+        """Initializing this square class
+        Args: size - represnets the size of the square defined
+        """
+
+        self.__size = size
